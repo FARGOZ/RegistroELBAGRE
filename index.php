dc6df0b7751b50743ee4cebd92beb97829b2a4b1
Farley Gonzalez <gonzalezfarley@hotmail.com>
2023-08-15 15:42:59 -0500

    15 de agosto

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenidos al app El Bagre</title>
+    <link rel="stylesheet" href="assets/estilos.css">
+</head>
+<body>
+    <?php require 'partials/header.php' ?>
+
+    <h1>Bienvenidos al aplicativo El Bagre</h1>
+    <h2>Por favor ingresa o registrate</h2>
+
+    <a href="login.php">Iniciar sesión</a> ó
+    <a href="registro.php">Regístrarse</a>
+    <footer><p>Aplicativo creado: por Farley Gonzalez</p></footer>
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenidos al app El Bagre</title>
+    <link rel="stylesheet" href="assets/estilos.css">
+</head>
+<body>
+    <header><a href="index.php">Aplicativo El Bagre</a></header>
+</body>
+</html>
