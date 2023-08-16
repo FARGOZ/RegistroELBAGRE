3c611bb617ebb89633221a85843aa19940084878
Farley Gonzalez <gonzalezfarley@hotmail.com>
2023-08-16 07:41:16 -0500

    Act 16 agosto 2013

---
+++ registro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenidos al app El Bagre</title>
+    <link rel="stylesheet" href="assets/estilos.css">
+</head>
+<body>
+    <?php require 'partials/header.php' ?>
+    
+    <h1>Registrarse</h1>
+    <form action="registro.php" method="post">
+        <input type="text" class="input" name="email" placeholder="Ingrese su correo">
+        <input type="password" class="input" name="password" placeholder="Ingrese su contraseña">
+        <input type="submit" class="boton" value="Enviar">
+    </form>
+    
+</body>
+</html>
